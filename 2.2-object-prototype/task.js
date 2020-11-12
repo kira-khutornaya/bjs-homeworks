b02a3d46878939d14969058f7bcaa8e6d34e076e
Kira Khutornaya <kira.khutornaya@mail.ru>
2020-11-12 19:33:51 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,41 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict";
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function() {
+    let prepped = this.toLowerCase().split(' ').join('');
+    if (prepped === prepped.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+//console.log("А роза упала на лапу Азора".isPalindrome());
+
+function getAverageMark(marks) {
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    for(let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    const average = sum / marks.length;
+    const roundedAverage = Math.round(average);
+
+    return roundedAverage;
+}
+
+function checkBirthday(dateOfBirthday) {
+    const now = Date.now();
+    const birthday = +new Date(dateOfBirthday);
+    const diff = now - birthday;
+    const age = diff / 3.15576e10; // среднее количество миллисекунд в году
+
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;    	
+    }
 }
